e0028df87c6dd43f03b15623a40ff72162654cdb
SerhiiQA <nabukve@gmail.com>
2025-03-20 18:27:26 +0200

    new run

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://telnyx.com",
+    projectId: "rzfw8y",
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
   },
 });
